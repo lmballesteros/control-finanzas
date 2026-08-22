b7dc78452e81bccc343b88daadac7718a9597a61
lmballesteros <luismballesterosn@gmail.com>
2026-08-22 16:09:43 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    si no hay red, esas llamadas simplemente fallarán con su propio manejo de error,
    pero el resto de la app sigue funcionando con normalidad. */
 
-var CACHE_NAME = "control-financiero-v5";
+var CACHE_NAME = "control-financiero-v6";
 var APP_SHELL = [
   "./",
   "./index.html",
